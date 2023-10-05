5d86dc5d44f8373701f6fe98ca9a8530e47ed16a
Oskar <oskdah-9@student.ltu.se>
2023-10-05 15:18:04 +0200

    Basic frontend for displaying the basic functions, working with backend api

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import TranscriptionDisplay from './TranscriptionDisplay';
+import Upload from './Upload'
+import './App.css';
+import logo from './Doris_logo_trans.png'
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className="navbar-logo" />
+      <Link to="/" className="nav-item">Home</Link>
+      <Link to="/upload" className="nav-item">Upload & Process</Link>
+      <Link to="/display" className="nav-item">Transcription Display</Link>
+    </nav>
+  );
+}
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar /> {/* Navbar added here will be on top */}
+        <Routes>
+          <Route path="/" element={
+            <>
+              <h1>Welcome to DoRiS</h1>
+              <div>DoRiS is a tool for transcribing and Diarizing your recordings</div>
+              <img src={logo} alt="Logo" className="home-page-logo" />
+            </>
+          } />
+          <Route path="/display" element={<TranscriptionDisplay />} />
+          <Route path="/upload" element={<Upload />}/>
+
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
---
+++ frontend/src/TranscriptionDisplay.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import penIcon from './penIcon.svg';
+
+
+function TranscriptionDisplay() {
+  const [fileList, setFileList] = useState([]);
+  const [transcriptionData, setTranscriptionData] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null); 
+  const [speakerMap, setSpeakerMap] = useState({}); 
+  const [editingSpeaker, setEditingSpeaker] = useState(null);
+
+  // Fetch the list of files when the page loads
+  useEffect(() => {
+    const fetchFileList = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/media');
+        if (response.ok) {
+          const data = await response.json();
+          setFileList(data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching file list.', error);
+      }
+    };
+
+    fetchFileList();
+  }, []);
+
+  // Fetch the content of a file when clicked
+  const fetchFileContent = async (mediaId) => {
+    try {
+      const response = await fetch(`http://localhost:8080/media/${mediaId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setTranscriptionData(data);
+        setSelectedFile(mediaId);
+      }
+    } catch (error) {
+      console.error('Error fetching file content.', error);
+    }
+  };
+
+
+  const formatTime = (seconds) => {
+    const roundedSeconds = Math.round(seconds);
+    const minutes = Math.floor(roundedSeconds / 60).toString().padStart(2, '0');
+    const remainingSeconds = (roundedSeconds % 60).toString().padStart(2, '0');
+    return `${minutes}.${remainingSeconds}`;
+  };
+
+  const handleLabelChange = (originalSpeaker, newLabel) => {
+    setSpeakerMap(prevState => ({
+      ...prevState,
+      [originalSpeaker]: newLabel
+    }));
+    setEditingSpeaker(null); // Close the input after editing
+  };
+
+  let previousSpeaker = null;
+
+  return (
+    <div className="App">
+      
+
+      <h1>Transcription App</h1>
+      {!selectedFile && (
+        <div>
+          <h2>Available Files</h2>
+          <ul className="file-list">
+            {fileList.map(file => (
+              <li key={file._id} onClick={() => fetchFileContent(file._id)}>
+                {file.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <div>
+        {transcriptionData.map((item, index) => {
+          const currentSpeaker = speakerMap[item.speaker] || item.speaker;
+          const showSpeaker = currentSpeaker !== previousSpeaker;
+          previousSpeaker = currentSpeaker;
+          return (
+            <div key={index} style={{ marginBottom: '30px' }}>
+              {showSpeaker && (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <h3>{currentSpeaker}:</h3>
+                  <button className="edit-button" onClick={() => setEditingSpeaker(item.speaker)}>
+                    <img src={penIcon} alt="Edit" width="16" height="16" />
+                  </button>
+
+
+                  {editingSpeaker === item.speaker && (
+                    <input
+                      className="label-input"
+                      defaultValue={currentSpeaker}
+                      onBlur={(e) => handleLabelChange(item.speaker, e.target.value)}
+                    />
+                  )}
+                </div>
+              )}
+              <p><span className="time">{formatTime(item.start)}:</span> {item.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TranscriptionDisplay;
