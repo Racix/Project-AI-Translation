713ed3f733fec777d175db2cf2a6d3d4798ad983
Oskar Dahlbeck <oskdah-9@student.ltu.se>
2023-11-02 12:08:19 +0100

    Editing in trans-display

--- frontend/src/TranscriptionDisplay.js
+++ frontend/src/TranscriptionDisplay.js
@@ -9,9 +9,17 @@ function TranscriptionDisplay() {
   const [selectedFile, setSelectedFile] = useState(null); 
   const [speakerMap, setSpeakerMap] = useState({}); 
   const [editingSpeaker, setEditingSpeaker] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const [editingIndex, setEditingIndex] = useState(null); 
+  const [tempSpeaker, setTempSpeaker] = useState(""); 
+  const [tempText, setTempText] = useState(""); 
+
+  const [editingLabelIndex, setEditingLabelIndex] = useState(null); // Track the index of the label being edited
+  const [tempLabel, setTempLabel] = useState(""); // Temporarily store the new label during editing
+
 
   const BASE_URL = process.env.REACT_APP_BACKEND_URL;
-
   // Fetch the list of files when the page loads
   useEffect(() => {
     console.log(process.env.REACT_APP_BACKEND_URL);
@@ -45,7 +53,65 @@ function TranscriptionDisplay() {
     }
 };
 
+const uploadTranscriptionEdits = async (mediaId) => {
+  const payload = {
+    "diarization.segments": transcriptionData
+  };
+  console.log(JSON.stringify(payload))
+  try {
+    const response = await fetch(`http://${BASE_URL}/media/${mediaId}/analysis`, {
+      method: 'PATCH', // or 'POST' if you are creating a new entry
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
 
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Handle a successful response
+    const result = await response.json();
+    console.log('Success:', result);
+  } catch (error) {
+    console.error('Error uploading transcription edits:', error);
+  }
+};
+
+const startEditing = (index, speaker, text) => {
+  setEditingIndex(index);
+  setTempSpeaker(speaker);
+  setTempText(text);
+};
+
+const saveText = (index) => {
+  // Save the text to your state or backend
+  const updatedTranscriptionData = [...transcriptionData];
+  updatedTranscriptionData[index] = { ...updatedTranscriptionData[index], text: tempText };
+  setTranscriptionData(updatedTranscriptionData);
+  setEditingIndex(null);
+};
+
+const handleChangeCurrentSpeakerLabelConfirm = () => {
+  const updatedTranscriptionData = transcriptionData.map((segment) => {
+    if (segment.speaker === editingSpeaker) {
+      return { ...segment, speaker: tempSpeaker };
+    }
+    return segment;
+  });
+  setTranscriptionData(updatedTranscriptionData);
+  setEditingSpeaker(null); // Exit editing mode
+};
+
+const handleChangeAllSpeakerLabelsConfirm = (index, newLabel) => {
+  setTranscriptionData(prevTranscriptionData =>
+    prevTranscriptionData.map((segment, currentIndex) =>
+      currentIndex === index ? { ...segment, speaker: newLabel } : segment
+    )
+  );
+  setEditingSpeaker(null); // Exit editing mode
+};
 
   const formatTime = (seconds) => {
     const roundedSeconds = Math.round(seconds);
@@ -62,6 +128,19 @@ function TranscriptionDisplay() {
     setEditingSpeaker(null); // Close the input after editing
   };
 
+  const startEditingLabel = (index, currentLabel) => {
+    setEditingLabelIndex(index);
+    setTempLabel(currentLabel); // Set the current label to the temporary state
+  };
+  
+  const changeLabel = (index) => {
+    setTranscriptionData(prevData => prevData.map((item, idx) =>
+      idx === index ? { ...item, speaker: tempLabel } : item
+    ));
+    setEditingLabelIndex(null); // Exit label editing mode
+  };
+
+  
   let previousSpeaker = null;
 
   return (
@@ -83,8 +162,13 @@ function TranscriptionDisplay() {
       )}
 
 <div>
-
+{selectedFile && (
+      <button onClick={() => uploadTranscriptionEdits(selectedFile)} className="save-button">
+        Save Changes
+      </button>
+    )}
   {transcriptionData && transcriptionData.map((item, index) => {
+    
     const currentSpeaker = speakerMap[item.speaker] || item.speaker;
     const showSpeaker = currentSpeaker !== previousSpeaker;
     previousSpeaker = currentSpeaker;
@@ -96,22 +180,54 @@ function TranscriptionDisplay() {
             <button className="edit-button" onClick={() => setEditingSpeaker(item.speaker)}>
               <img src={penIcon} alt="Edit" width="16" height="16" />
             </button>
-
             {editingSpeaker === item.speaker && (
+              <>
               <input
                 className="label-input"
                 defaultValue={currentSpeaker}
                 onBlur={(e) => handleLabelChange(item.speaker, e.target.value)}
               />
+               <button className="change-label-button" onClick={() => handleChangeCurrentSpeakerLabelConfirm(index, tempSpeaker)}>
+          Change this label
+        </button>
+        <button className="change-all-labels-button" onClick={() => handleChangeAllSpeakerLabelsConfirm()}>
+          Change all labels
+        </button>
+              </> 
             )}
           </div>
         )}
-        <p><span className="time">{formatTime(item.start)}:</span> {item.text}</p>
+        <p className="transcription-text"
+     onMouseEnter={() => setHoveredIndex(index)}
+     onMouseLeave={() => setHoveredIndex(null)}
+  >
+
+    <span className="time">{formatTime(item.start)}:</span>
+    
+    {editingIndex === index ? (
+      <input
+        className="text-input"
+        value={tempText}
+        onChange={(e) => setTempText(e.target.value)}
+        onBlur={() => saveText(index)}
+      />
+    ) : (
+      <span>{item.text}</span>
+    )}
+    {hoveredIndex === index && editingIndex !== index && (
+      <button 
+        className="edit-button-style" 
+        onClick={() => startEditing(index, item.speaker, item.text)}
+      >
+        <img src={penIcon} alt="Edit" width="16" height="16" />
+      </button>
+    )}
+  </p>
       </div>
     );
   })}
-</div>
     </div>
+  </div>
   );
 }
 
