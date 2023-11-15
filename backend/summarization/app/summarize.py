4b00d8e9b1c6380a5dae99c9f84cc374cf76a68e
tonytonfisk2 <tony6781@hotmail.com>
2023-11-15 22:23:41 +0100

    predownload model, preprocessing

--- backend/summarization/app/summarize.py
+++ backend/summarization/app/summarize.py
@@ -1,79 +1,81 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, load_index_from_storage, SummaryIndex
-from transformers import BitsAndBytesConfig
-from llama_index.prompts import PromptTemplate
-from llama_index.llms import HuggingFaceLLM
-from llama_index.response.notebook_utils import display_response
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.query_engine import RouterQueryEngine
-import torch
+from llama_index import ListIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import (
+    completion_to_prompt,
+)
+import json, tempfile, os
 
 def load_data(file_path: str):
-    reader = SimpleDirectoryReader(input_files=[file_path])
+    with open(file_path, 'r') as file:
+        json_data = file.read()
+    
+    json_data = json.loads(json_data)
+    segments = json_data['segments']
+    text_data = ' '.join([segment['text'] for segment in segments])
+ 
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
+        # Write data to the temporary file
+        temp_file.write(text_data)
+        temp_file_path = temp_file.name  # Store the path of the temporary file
+
+    # Load data using SimpleDirectoryReader
+    reader = SimpleDirectoryReader(input_files=[temp_file_path])
     documents = reader.load_data()
+    os.remove(temp_file_path)
     return documents
 
 def messages_to_prompt(messages):
   prompt = ""
   for message in messages:
     if message.role == 'system':
-      prompt += f"<|system|>\n{message.content}</s>\n"
+      prompt += f"<|im_start|>system\n{message.content}<im_end>\n"
     elif message.role == 'user':
-      prompt += f"<|user|>\n{message.content}</s>\n"
+      prompt += f"<|im_start|>user\n{message.content}<im_end>\n"
     elif message.role == 'assistant':
-      prompt += f"<|assistant|>\n{message.content}</s>\n"
+      prompt += f"<|im_start|>assistant\n{message.content}<im_end>\n"
 
   # ensure we start with a system prompt, insert blank if needed
-  if not prompt.startswith("<|system|>\n"):
-    prompt = "<|system|>\n</s>\n" + prompt
+  if not prompt.startswith("<|im_start|>system\n"):
+    prompt = "<|im_start|>\n<im_end>\n" + prompt
 
   # add final assistant prompt
-  prompt = prompt + "<|assistant|>\n"
+  prompt = prompt + "<|im_start|>assistant\n"
 
   return prompt
 
 def model():
-    print("model1")
-    quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
-    print("model2")
-    llm = HuggingFaceLLM(
-    model_name="HuggingFaceH4/zephyr-7b-beta",
-    tokenizer_name="HuggingFaceH4/zephyr-7b-beta",
-    query_wrapper_prompt=PromptTemplate("<|system|>\n</s>\n<|user|>\n{query_str}</s>\n<|assistant|>\n"),
-    context_window=3900,
+    llm = LlamaCPP(
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path="models/mistral7b",
+    temperature=0.1,
     max_new_tokens=256,
-    model_kwargs={"quantization_config": quantization_config},
-    # tokenizer_kwargs={},
-    generate_kwargs={"temperature": 0.7, "top_k": 50, "top_p": 0.95},
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=3900,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": -1},
+    # transform inputs into Llama2 format
     messages_to_prompt=messages_to_prompt,
-    device_map="auto",
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
 )
-    print("model3")
     return llm
     
 def create_index(llm, documents):
-    print("create_summarize4\n")
-    service_context = ServiceContext.from_defaults(llm=llm, embed_model="local:BAAI/bge-base-en-v1.5", chunk_size = 1024)
-    print("create_summarize5\n")
-    vector_index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-    print("create_summarize6\n")
-    return vector_index
+    service_context = ServiceContext.from_defaults(llm=llm, embed_model="local:BAAI/bge-base-en-v1.5", context_window = 3700)
+    list_index = ListIndex.from_documents(documents, service_context=service_context)
+    return list_index
     
-def summarize(vector_index):
-    query_engine = vector_index.as_query_engine(response_mode="tree_summarize")
+def summarize(list_index):
+    query_engine = list_index.as_query_engine(response_mode="tree_summarize")
     response = query_engine.query("Can you summarize the text for me?")
     return response
 
 def create_summarize(file_path: str):
-    print("create_summarize1\n")
     documents = load_data(file_path)
-    print("create_summarize2\n")
     llm = model()
-    print("create_summarize3\n")
-    vector_index = create_index(llm, documents)
-    print("create_summarize7\n")
-    return summarize(vector_index)
+    list_index = create_index(llm, documents)
+    return summarize(list_index)
