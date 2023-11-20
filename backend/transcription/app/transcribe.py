350ca03f20cc29c2402be051a2ed7e08242da611
Hamid <hamidehsani54@gmail.com>
2023-11-20 10:38:36 +0000

    new version of transcription and language detection

--- backend/transcription/app/transcribe.py
+++ backend/transcription/app/transcribe.py
@@ -2,18 +2,17 @@ from transformers import pipeline
 from langdetect import detect
 import time 
 import traceback
+import os
 
-MODEL_NAME3 = "openai/whisper-small"
+MODEL_NAME = "openai/whisper-small"
 model_size = "tiny"
 
 
 def transcribe(file_path: str) -> dict:
-    try:
-
-        
+    try:        
         whisper_pipeline = pipeline(
             "automatic-speech-recognition",
-            model=MODEL_NAME3,
+            model=MODEL_NAME,
             chunk_length_s=30,
             device="cpu",
         )
@@ -21,29 +20,26 @@ def transcribe(file_path: str) -> dict:
         To_detect_language = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={"task": "transcribe"})
         elements = To_detect_language['chunks']
         Text_To_be_detect = ' '.join([chunk['text'] for chunk in elements])
-        detected_language = detect(Text_To_be_detect)
-        
+        detected_language = detect(Text_To_be_detect) 
+                
         #Transcribe to english
         transcription = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={ "language": "en", "task": "transcribe"})        
         chunks = transcription['chunks']
-        transcription_data = []
-        
-        # Extract transcribed text for language detection
+        transcription_data = []        
         transcribed_text = ' '.join([chunk['text'] for chunk in chunks])
     
         for chunk in chunks:
-            start_timestamp, end_timestamp = chunk.get('timestamp', (None, None))
-            
-            if start_timestamp is not None and end_timestamp is not None:
-                duration = end_timestamp - start_timestamp
+            start_timestamp, end_timestamp = chunk['timestamp']
+            text = chunk['text']
+            duration = end_timestamp - start_timestamp
 
-                sentence = {
-                    "text": chunk['text'],
-                    "start": start_timestamp,
-                    "duration": duration,
-                }
-                transcription_data.append(sentence)
-            
+            sentence = {
+                'text': text,
+                'start': start_timestamp,
+                'duration': duration,
+                'Speaker': "Speaker0"
+            }
+            transcription_data.append(sentence)      
         result_dict = {
             "segments": transcription_data,
             "detected_language": detected_language,
