b1418d36c98bde88a257ed5a5eec7c4f5e4377f2
IsakLundstrom <isalun-9@student.ltu.se>
2023-11-21 12:06:38 +0100

    Can now chose sample rate and channels, set at 16k and 1

--- sound_driver/sound_driver.py
+++ sound_driver/sound_driver.py
@@ -1,4 +1,5 @@
 import pyaudiowpatch as pyaudio
+import librosa
 import time
 import wave
 import numpy as np
@@ -13,19 +14,20 @@ async def sound_driver():
 
     # Set the audio parameters
     FORMAT = pyaudio.paInt16
-    CHUNK = 512
-    RECORD_SECONDS = 10 # You can adjust the recording duration as needed
+    # CHUNK = 512
+    # RECORD_SECONDS = 10 # You can adjust the recording duration as needed
     OUTPUT_FILENAME = "test.wav"
 
     audio = pyaudio.PyAudio()
 
     SAMPLE_SIZE = audio.get_sample_size(FORMAT)
-    print(SAMPLE_SIZE)
+    # print(SAMPLE_SIZE)
 
     wasapi_info = audio.get_host_api_info_by_type(pyaudio.paWASAPI)
     print(wasapi_info)
+
     default_speakers = audio.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
-    default_speakers = audio.get_device_info_by_index(22)
+    # default_speakers = audio.get_device_info_by_index(22)
     default_mic = audio.get_device_info_by_index(wasapi_info["defaultInputDevice"])
     print("default_speakers:", default_speakers["name"])
     print("default_mic:", default_mic["name"])
@@ -49,31 +51,37 @@ async def sound_driver():
     # number of channels to be higher or equalt to the max amount of channels or else weird stuff happend to the audio quality
     # sample_rate = default_speakers["defaultSampleRate"] if default_speakers["defaultSampleRate"] >= default_mic["defaultSampleRate"] else default_mic["defaultSampleRate"]
     sample_rate = int(min(default_speakers["defaultSampleRate"], default_mic["defaultSampleRate"]))
-    # sample_rate = 
     n_channels = default_speakers["maxInputChannels"] if default_speakers["maxInputChannels"] >= default_mic["maxInputChannels"] else default_mic["maxInputChannels"]
-    print("samle_rate", sample_rate, "n_channels", n_channels)
-
-    speaker_file = wave.open(OUTPUT_FILENAME, 'wb')
-    speaker_file.setnchannels(n_channels)
-    speaker_file.setsampwidth(SAMPLE_SIZE)
-    speaker_file.setframerate(sample_rate)   
+    # print("samle_rate", SAMPLE_RATE, "n_channels", n_channels)
     
-    CHUNK = int(sample_rate / 100) * 2
+    SAMPLE_RATE = 16000
+    NUMBER_CHANNELS = 1
+    SPEAKER_RATIO = default_speakers["defaultSampleRate"] / SAMPLE_RATE
+    MIC_RATIO = default_mic["defaultSampleRate"] / SAMPLE_RATE
+    CHUCK_DURATION = 30 # ms
+    SPEAKER_CHUNK = int(CHUCK_DURATION * SAMPLE_RATE * SPEAKER_RATIO / 1000) * default_speakers["maxInputChannels"]
+    MIC_CHUNK = int(CHUCK_DURATION * SAMPLE_RATE * MIC_RATIO / 1000) * default_mic["maxInputChannels"]
+    print(f"SAMPLE_RATE: {SAMPLE_RATE}, NUMBER_CHANNELS: {NUMBER_CHANNELS}, SPEAKER_RATIO: {SPEAKER_RATIO}, MIC_RATIO: {MIC_RATIO}, CHUNK_DURATION: {CHUCK_DURATION}, SPEAKER_CHUNK: {SPEAKER_CHUNK}, MIC_CHUNK: {MIC_CHUNK}")
     # 48000 / 100 = 480   1s = 480000   0.01s =  480  
     # 44100 / 100 = 441   
 
+    speaker_file = wave.open(OUTPUT_FILENAME, 'wb')
+    speaker_file.setnchannels(NUMBER_CHANNELS)
+    speaker_file.setsampwidth(SAMPLE_SIZE)
+    speaker_file.setframerate(SAMPLE_RATE)   
+
     speaker_stream = audio.open(format=FORMAT,
-        channels=n_channels,
-        rate=sample_rate,
-        frames_per_buffer=CHUNK,
+        channels=default_speakers["maxInputChannels"],
+        rate=int(default_speakers["defaultSampleRate"]),
+        frames_per_buffer=SPEAKER_CHUNK,
         input=True,
         input_device_index=default_speakers["index"],
     )
 
     mic_stream = audio.open(format=FORMAT, 
-        channels=n_channels,
-        rate=sample_rate, 
-        frames_per_buffer=CHUNK,
+        channels=default_mic["maxInputChannels"],
+        rate=int(default_mic["defaultSampleRate"]), 
+        frames_per_buffer=MIC_CHUNK,
         input=True
     )
 
@@ -91,61 +99,104 @@ async def sound_driver():
     test_file_counter = 0
 
     started_talking = False
-    while 1== 1:
-        # Read data from speaker and mic
-        # speaker_data = speaker_stream.read(CHUNK)
-        mic_data = mic_stream.read(CHUNK, exception_on_overflow=False)
+    try:
+        while 1== 1:
+            # Read data from speaker and mic
+            speaker_data = speaker_stream.read(SPEAKER_CHUNK)
+            mic_data = mic_stream.read(MIC_CHUNK, exception_on_overflow=False)
+            # print(speaker_data)
 
-        # speaker_data = np.frombuffer(speaker_data, dtype=np.int16)
-        # print("speaker", speaker_data.size)
-        mic_data = np.frombuffer(mic_data, dtype=np.int16)
-        # print("mic", mic_data.size)
-        # combined_data = speaker_data + mic_data
-        combined_data = mic_data
+            speaker_data = np.fromstring(speaker_data, dtype=np.int16).astype(np.float32)
+            mic_data = np.fromstring(mic_data, dtype=np.int16).astype(np.float32)
+            # print("Original speaker stats:", speaker_data.shape, np.min(speaker_data), np.max(speaker_data), np.mean(speaker_data))
+            # print("Original mic stats:", mic_data.shape, np.min(mic_data), np.max(mic_data), np.mean(mic_data))
 
-        speaker_file.writeframes(combined_data)
 
-        if not is_talking(combined_data, sample_rate):
+            speaker_data = np.reshape(speaker_data, (default_speakers["maxInputChannels"], SPEAKER_CHUNK))
+            mic_data = np.reshape(mic_data, (default_mic["maxInputChannels"], MIC_CHUNK))
+            # print("Reshaped speaker stats:", speaker_data.shape, np.min(speaker_data), np.max(speaker_data), np.mean(speaker_data))
+            # print("Reshaped mic stats:", mic_data.shape, np.min(mic_data), np.max(mic_data), np.mean(mic_data))
 
-            if started_talking:
-                send_data = np.append(send_data, combined_data)
 
-            # if voice_activity_last_loop:
-            loop_num+= 1
-            if loop_num >= 50 and send_data.size >= 50000:
-                send_data = np.insert(send_data, 0, int(sample_rate/100))
-                send_data = np.insert(send_data, 0, n_channels)
+            speaker_data = librosa.resample(speaker_data, orig_sr=default_speakers["defaultSampleRate"], target_sr=SAMPLE_RATE)
+            mic_data = librosa.resample(mic_data, orig_sr=default_mic["defaultSampleRate"], target_sr=SAMPLE_RATE)
+            # print("Resample speaker stats:", speaker_data.shape, np.min(speaker_data), np.max(speaker_data), np.mean(speaker_data))
+            # print("Resample mic stats:", mic_data.shape, np.min(mic_data), np.max(mic_data), np.mean(mic_data))
 
-                # write each sent audio
-                test_file = wave.open(f"test/test_{test_file_counter}.wav", 'wb')
-                test_file.setnchannels(n_channels)
-                test_file.setsampwidth(SAMPLE_SIZE)
-                test_file.setframerate(sample_rate)  
-                test_file.writeframes(send_data[2:])
-                test_file.close()
-                test_file_counter += 1 
-                            
-                asyncio.create_task(ws.send_audio(audio=send_data.tobytes()))
-                # print("send audio")
-                started_talking = False
-                send_data = np.empty(0, dtype=np.int16)
-                await asyncio.sleep(0.01)
+            speaker_data = np.mean(speaker_data.reshape(-1, default_speakers["maxInputChannels"]), axis=1).astype(np.int16)
+            mic_data = np.mean(mic_data.reshape(-1, default_mic["maxInputChannels"]), axis=1).astype(np.int16)
+            # print("Mono speaker stats:", speaker_data.shape, np.min(speaker_data), np.max(speaker_data), np.mean(speaker_data))
+            # print("Mono mic stats:", mic_data.shape, np.min(mic_data), np.max(mic_data), np.mean(mic_data))
 
-            print(".", end="")
-            # voice_activity_last_loop = False
+            # await asyncio.sleep(10)
 
-            continue
-        
-        print("!", end="")
-        # voice_activity_last_loop = True
-        # print("Audio!")
-        loop_num = 0
-        send_data = np.append(send_data, combined_data)
-        started_talking  = True
+            max_length = max(len(speaker_data), len(mic_data))
 
-    ws.close()
-    speaker_file.close()
-    print("Done!")
+            # Pad the arrays to the same size
+            speaker_data = np.pad(speaker_data, (0, max_length - len(speaker_data)), mode='constant').astype(np.int16)
+            mic_data = np.pad(mic_data, (0, max_length - len(mic_data)), mode='constant').astype(np.int16)
+
+            combined_data = speaker_data + mic_data
+            # print("Combined stats:", combined_data.shape, np.min(combined_data), np.max(combined_data), np.mean(combined_data))
+
+            speaker_file.writeframes(combined_data)
+
+            if not is_talking(combined_data, SAMPLE_RATE):
+
+                if started_talking:
+                    send_data = np.append(send_data, combined_data).astype(np.int16)
+
+                # if voice_activity_last_loop:
+                loop_num+= 1
+                if loop_num >= 50 and send_data.size >= 50000:
+                    # send_data = np.insert(send_data, 0, int(SAMPLE_RATE/100)).astype(np.int16)
+                    # send_data = np.insert(send_data, 0, NUMBER_CHANNELS).astype(np.int16)
+                    # np.savetxt(f"test/test_{test_file_counter}.txt", send_data)
+
+                    # write each sent audio
+                    test_file = wave.open(f"test/test_{test_file_counter}.wav", 'wb')
+                    test_file.setnchannels(NUMBER_CHANNELS)
+                    test_file.setsampwidth(SAMPLE_SIZE)
+                    test_file.setframerate(SAMPLE_RATE)  
+                    # test_file.writeframes(send_data[2:])
+                    test_file.writeframes(send_data)
+                    test_file.close()
+                    test_file_counter += 1 
+                                
+                    asyncio.create_task(ws.send_audio(audio=send_data.tobytes()))
+                    # print("send audio")
+                    started_talking = False
+                    send_data = np.empty(0, dtype=np.int16)
+                    loop_num = 0
+                    await asyncio.sleep(0.01)
+
+                print(".", end="")
+                # voice_activity_last_loop = False
+
+                continue
+            
+            print("!", end="")
+            # voice_activity_last_loop = True
+            # print("Audio!")
+            loop_num = 0
+            send_data = np.append(send_data, combined_data)
+            started_talking  = True
+
+    except KeyboardInterrupt:
+        # Graceful exit on keyboard interrupt
+        pass
+    finally:
+        # Stop and close
+        speaker_stream.stop_stream()
+        speaker_stream.close()
+        mic_stream.stop_stream()
+        mic_stream.close()
+        audio.terminate()
+
+        speaker_file.close()
+        await ws.close()
+        print("Done!")
+
 
 
 
