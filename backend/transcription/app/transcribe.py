3111a87a9f00bd871c754b239dfe9441454f8d59
Hamid <hamidehsani54@gmail.com>
2023-11-22 15:08:50 +0000

    Everything should work now

--- backend/transcription/app/transcribe.py
+++ backend/transcription/app/transcribe.py
@@ -2,31 +2,25 @@ from transformers import pipeline
 from langdetect import detect
 import time 
 import traceback
-import os
+
 
 MODEL_NAME = "openai/whisper-small"
 model_size = "tiny"
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def transcribe(file_path: str) -> dict:
     try:        
         whisper_pipeline = pipeline(
             "automatic-speech-recognition",
             model=MODEL_NAME,
-            chunk_length_s=30,
-            device="cpu",
-        )
-        #Used to detect the original language of the file
-        transcription_result = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={"task": "transcribe"})
-        transcribed_text = ' '.join([chunk['text'] for chunk in transcription_result['chunks']])
-        detected_language = detect(transcribed_text)
-                
-        #Transcribe to english
-        transcription = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={ "language": "en", "task": "transcribe"})        
+            chunk_length_s=30,       
+        )                
+        #Transcribe the video to the original language
+        transcription = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={"task": "transcribe"})        
         chunks = transcription['chunks']
         transcription_data = []        
         transcribed_text = ' '.join([chunk['text'] for chunk in chunks])
-    
+        detected_language = detect(transcribed_text)
         for chunk in chunks:
             start_timestamp, end_timestamp = chunk['timestamp']
             text = chunk['text']
@@ -40,9 +34,10 @@ def transcribe(file_path: str) -> dict:
             }
             transcription_data.append(sentence)      
         result_dict = {
-            "segments": transcription_data,
             "detected_language": detected_language,
+            "segments": transcription_data,            
         }
+        print(result_dict)
         return result_dict
     except Exception as e:
         print(traceback.format_exc())
