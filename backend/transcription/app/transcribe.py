8befed38b948a3f7c095c9d8f3a93d6731036a1f
Hamid Ehsani <hamidehsani54@gmail.com>
2023-12-06 13:32:03 +0000

    Everything should work now!

--- backend/transcription/app/transcribe.py
+++ backend/transcription/app/transcribe.py
@@ -6,8 +6,9 @@ from loadModel import whisper_pipeline
 
 
 def transcribe(file_path: str) -> dict:
-    try:                        
-        #Transcribe the video to the original language
+    try:         
+        print(f"Transcription of {file_path} started...")               
+        # Transcribe the video to the original language
         transcription = whisper_pipeline(file_path, return_timestamps=True, chunk_length_s=30, batch_size=32, generate_kwargs={"task": "transcribe"})        
         chunks = transcription['chunks']
         transcription_data = []        
@@ -15,16 +16,20 @@ def transcribe(file_path: str) -> dict:
         detected_language = detect(transcribed_text)
         for chunk in chunks:
             start_timestamp, end_timestamp = chunk['timestamp']
-            text = chunk['text']
-            duration = end_timestamp - start_timestamp
+            
+            # Check if end_timestamp is None
+            if end_timestamp is not None:
+                text = chunk['text']
+                duration = end_timestamp - start_timestamp
 
-            sentence = {
-                'text': text,
-                'start': start_timestamp,
-                'duration': duration,
-                'Speaker': "Speaker0"
-            }
-            transcription_data.append(sentence)      
+                sentence = {
+                    'text': text,
+                    'start': start_timestamp,
+                    'duration': duration,
+                    'Speaker': "Speaker0"
+                }
+                transcription_data.append(sentence)      
+            
         result_dict = {
             "detected_language": detected_language,
             "segments": transcription_data,            
