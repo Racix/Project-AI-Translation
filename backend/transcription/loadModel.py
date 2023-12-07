790b82e615833941b8a7b0cafa2dc713994e0995
tonytonfisk2 <tony6781@hotmail.com>
2023-12-07 22:59:45 +0100

    testing pipeline medium

--- backend/transcription/loadModel.py
+++ backend/transcription/loadModel.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 import torch
 
-MODEL_NAME = "openai/whisper-large-v2"
+MODEL_NAME = "openai/whisper-medium"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 whisper_pipeline = pipeline(
